5b82222dc9b816a2ba829d0dc8800fcd70cf5724
Kristian309 <163397278+Kristian309@users.noreply.github.com>
2026-09-16 23:43:28 +0300

    54

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.uav.app',
+  appName: 'UAV_TACK',
+  webDir: 'dist'
+};
+
+export default config;
